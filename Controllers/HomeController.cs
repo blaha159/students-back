44f76ddcc459e6e2b95ef59bb19447b53b36cb3d
blahapet <petr.blaha@cez.cz>
2021-04-30 15:57:10 +0200

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace students_back.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public RedirectResult RedirectToSwaggerUi()
+        {
+            return Redirect("/swagger/");
+        }
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace students_back.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("~/getWeatherForecast")]
+        public IEnumerable<WeatherForecast> getWeatherForecast()
+        {
+            var rng = new Random();
+            _logger.LogInformation("test logovani informace");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        /*[HttpPost("~/postJson")]
+        [Consumes("application/json")]
+        public IActionResult postJson(IEnumerable<int> values) =>
+            Ok(new { Consumes = "application/json", Values = values });
+
+        [HttpPost("~/postForm")]
+        [Consumes("application/x-www-form-urlencoded")]
+        public IActionResult postForm([FromForm] IEnumerable<int> values) =>
+            Ok(new { Consumes = "application/x-www-form-urlencoded", Values = values });*/
+    }
+}
